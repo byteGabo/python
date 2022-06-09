d2114cc50be854ec61d685b325b06cfda1df71d3
Gabriel Chaves <gabovvv@gmail.com>
2022-06-08 20:40:29 -0600

    Se cambio el if por un for para recorrer las filas y columnas del tablero y determinar si esta ocupado o no

--- tictactoe.py
+++ tictactoe.py
@@ -27,29 +27,33 @@ def EnterMove(board):
         elif str(move) not in board[0] and str(move) not in board[1] and str(move) not in board[2]:
             print('Lo siento, esa casilla ya esta tomada, por favor seleccione otra casilla')
             continue
-        elif move == 1:
-            board[0][0] = 'O'
-        elif move == 2:
-            board[0][1] = 'O'
-        elif move == 3:
-            board[0][2] = 'O'
-        elif move == 4:
-            board[1][0] = 'O'
-        elif move == 6:
-            board[1][2] = 'O'
-        elif move == 7:
-            board[2][0] = 'O'
-        elif move == 8:
-            board[2][1] = 'O'
-        elif move == 9:
-            board[2][2] = 'O'
         
+        for row in range(0,3):
+            for column in range(0,3):
+                if board[row][column] == str(move):
+                    board[row][column] = 'O'
         break
+
+def MakeListOfFreeFields(board):
+    free_square = []
+    
+    for row in range(0,3):
+        for column in range(0,3):
+            if board[row][column] == 'X' or board[row][column] == 'O':
+                pass
+            else:
+                free_square.append(([row],[column]))
+    
+    print(free_square)
+    
+def VictoryFor(board, sign):
+    pass    
 
 
 DisplayBoard(board)
 EnterMove(board)
 DisplayBoard(board)
+MakeListOfFreeFields(board)
 
 
 
